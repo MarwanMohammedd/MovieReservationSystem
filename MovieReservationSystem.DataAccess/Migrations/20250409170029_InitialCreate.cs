1ebe24472b3fb8b0277802e73735f729c4892609
Heba Omar <Hebaomarr9@gmail.com>
2025-04-10 02:23:23 +0200

    Save my changes before rebase

---
+++ MovieReservationSystem.DataAccess/Migrations/20250409170029_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieReservationSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartTime",
+                table: "MovieSchedules",
+                newName: "Showtime");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MovieSchedules_MovieId_StartTime",
+                table: "MovieSchedules",
+                newName: "IX_MovieSchedules_MovieId_Showtime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Showtime",
+                table: "MovieSchedules",
+                newName: "StartTime");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MovieSchedules_MovieId_Showtime",
+                table: "MovieSchedules",
+                newName: "IX_MovieSchedules_MovieId_StartTime");
+        }
+    }
+}
